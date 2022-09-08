adbf0d75fc9ecf16697177b3662568b04f6d91e2
betogroo <luizhumberto@gmail.com>
2022-09-08 14:15:45 -0300

    chore: first commit

---
+++ src/components/app/AppBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineEmits(['toggle-drawer'])
+</script>
+<template>
+  <v-app-bar color="primary">
+    <v-app-bar-nav-icon @click="$emit('toggle-drawer')"></v-app-bar-nav-icon>
+    <v-app-bar-title>
+      <router-link
+        class="text-white text-decoration-none"
+        :to="{ name: 'HomeView' }"
+      >
+        Vue Vite Project
+      </router-link>
+    </v-app-bar-title>
+    <v-btn icon>
+      <v-icon>mdi-dots-horizontal</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
