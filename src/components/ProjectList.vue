03355f1eec59eacf93e11accd224219dd630a986
betogroo <luizhumberto@gmail.com>
2022-09-10 11:13:31 -0300

    feat: add project card to project list

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import useProject from '@/composables/useProject'
+import ProjectCard from './ProjectCard.vue'
 const { projects, error, fetchProject } = useProject()
 await fetchProject()
 </script>
@@ -9,16 +10,20 @@ await fetchProject()
     <v-alert type="error">{{ `${error.code} - ${error.message}` }}</v-alert>
   </div>
   <template v-else-if="projects.length">
-    <v-card
-      :disabled="item.complete"
-      :border="true"
-      class="pa-1 my-2"
-      v-for="item in projects"
-      :key="item.id"
-    >
-      <v-card-title class="text-h6">{{ item.title }}</v-card-title>
-      <v-card-subtitle>{{ item.details }}</v-card-subtitle>
-    </v-card>
+    <ProjectCard
+      :item="project"
+      v-for="project in projects"
+      :key="project.id"
+    />
   </template>
   <v-alert v-else type="warning">No data</v-alert>
 </template>
+
+<style lang="scss" scoped>
+.card-border {
+  border-left: 5px solid red;
+}
+.complete {
+  border-left-color: green;
+}
+</style>
